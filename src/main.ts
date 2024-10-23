47f9f6453b21b222e92a4204e138338ff983acb5
Arthur Walters <arthur.agw@gmail.com>
2024-10-23 19:07:56 +0200

    update filter

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ import { bootstrapCameraKit } from "@snap/camera-kit";
         apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI4NDEzMTUyLCJzdWIiOiI2ZTY1NjkyNC1hYzc5LTQ2MDAtOWU1Zi02M2YyZWVhYWI2NjF-U1RBR0lOR344OWI0MGQ4Yi1jOTY0LTQ5OTUtOWFkNC00NThkMGEwNjVhYWEifQ.01jq2pPDI9iXylWl6UCBFWeCT3GzshZpkzCL6CCHo3A'
     });
 
+
     const liveRenderTarget = document.getElementById('canvas') as HTMLCanvasElement;
-    
-    // Spiegeling toevoegen via CSS-transformatie
-    liveRenderTarget.style.transform = 'scaleX(-1)';
+
 
     const session = await cameraKit.createSession({ liveRenderTarget });
 
@@ -18,11 +17,29 @@ import { bootstrapCameraKit } from "@snap/camera-kit";
         }
     });
 
-
     await session.setSource(mediaStream);
     await session.play();
 
-    const lens = await cameraKit.lensRepository.loadLens('9c456053-460c-4480-a600-0fbc5a97fe84', '8f1e9505-86ad-46eb-8552-ecff9d1be776');
+    const lens = await cameraKit.lensRepository.loadLens(
+        '4ae066a2-ac14-4d79-a290-0c26e46c6041',
+        '8f1e9505-86ad-46eb-8552-ecff9d1be776'
+    );
     await session.applyLens(lens);
+
+    // Voeg een event listener toe om de canvas-resolutie aan te passen bij resizes
+    function resizeCanvas() {
+        const ratio = liveRenderTarget.clientWidth / liveRenderTarget.clientHeight;
+        liveRenderTarget.width = liveRenderTarget.clientWidth;
+        liveRenderTarget.height = liveRenderTarget.clientHeight;
+
+        // Zorg ervoor dat de video-inhoud de juiste verhoudingen behoudt
+        session.setRenderSize(liveRenderTarget.width, liveRenderTarget.height);
+    }
+
+    // Initialiseer de canvas grootte
+    resizeCanvas();
+
+    // Pas de canvas aan bij vensterveranderingen
+    window.addEventListener('resize', resizeCanvas);
 
 })();
